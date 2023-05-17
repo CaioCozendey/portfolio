eec5379928bbf73b04af45f9cc6f9c045615422c
Caio Dias Cozendey <caiocozendey@hotmail.com>
2023-05-17 20:08:15 -0300

    dkasasdk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,7 @@ export default function Home() {
       <Header></Header>
       <Section></Section>
       <Section2></Section2>
-
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"/>
-          
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
     </div>
   )
 }
